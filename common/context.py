16ce4186548dd887eb14dff8dfb340173cb73fa9
Dan <9sarvas9@gmail.com>
2023-11-26 10:22:36 +0200

    style changes in populate_context

--- common/context.py
+++ common/context.py
@@ -25,11 +25,7 @@ def populate_context(spark, context):
     context["akas"] = load_dataframe(spark, AKAS_SCHEMA, "akas.tsv")
     context["crew"] = load_dataframe(spark, CREW_SCHEMA, "crew.tsv")
     context["episode"] = load_dataframe(spark, EPISODES_SCHEMA, "episode.tsv")
-    context["name_basics"] = load_dataframe(
-        spark, NAME_BASICS_SCHEMA, "name_basics.tsv"
-    )
+    context["name_basics"] = load_dataframe(spark, NAME_BASICS_SCHEMA, "name_basics.tsv")
     context["principals"] = load_dataframe(spark, PRINCIPALS, "principals.tsv")
     context["ratings"] = load_dataframe(spark, RATINGS_SCHEMA, "ratings.tsv")
-    context["title_basics"] = load_dataframe(
-        spark, TITLE_BASICS_SCHEMA, "title_basics.tsv"
-    )
+    context["title_basics"] = load_dataframe(spark, TITLE_BASICS_SCHEMA, "title_basics.tsv")
